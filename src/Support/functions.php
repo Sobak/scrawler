a6b2ce0412c4634e78799175c41848f39fe8504c
Sobak <msobaczewski@gmail.com>
2019-02-26 19:57:28 +0100

    Add fallback functions for dd/dump when no dev dependencies are installed

---
+++ src/Support/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+if (function_exists('dd') === false) {
+    function dd(...$vars)
+    {
+        foreach ($vars as $var) {
+            var_dump($var);
+        }
+
+        die(1);
+    }
+}
+
+if (function_exists('dump') === false) {
+    function dump(...$vars)
+    {
+        foreach ($vars as $var) {
+            var_dump($var);
+        }
+    }
+}
