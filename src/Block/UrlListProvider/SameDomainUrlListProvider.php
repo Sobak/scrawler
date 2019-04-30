7bce32c69d4280289fe485322a05e36537c9bc1f
Sobak <msobaczewski@gmail.com>
2019-04-30 16:09:44 +0200

    Fix incorrent base for resolving URLs

--- src/Block/UrlListProvider/SameDomainUrlListProvider.php
+++ src/Block/UrlListProvider/SameDomainUrlListProvider.php
@@ -13,15 +13,15 @@ class SameDomainUrlListProvider extends AbstractUrlListProvider
     public function getUrls(): array
     {
         $baseDomain = $this->baseUrl->getDomain();
-        $baseUrl = $this->baseUrl->getUrl();
+        $currentUrl = $this->currentUrl->getUrl();
 
         // Make sure that we have contents to read
         $this->response->getBody()->rewind();
 
         $responseCrawler = new Crawler($this->response->getBody()->getContents());
         $linkTags = $responseCrawler->filter('a')->getIterator()->getArrayCopy();
-        $urls = array_map(function (DOMElement $link) use ($baseUrl) {
-            return $link->hasAttribute('href') ? new Url($link->getAttribute('href'), $baseUrl) : null;
+        $urls = array_map(function (DOMElement $link) use ($currentUrl) {
+            return $link->hasAttribute('href') ? new Url($link->getAttribute('href'), $currentUrl) : null;
         }, $linkTags);
 
         return array_filter($urls, function (?Url $url) use ($baseDomain) {
